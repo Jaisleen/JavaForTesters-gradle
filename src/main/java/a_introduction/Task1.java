68bb2049cef313ffe10d8e413e180d4b60f476e0
hjaissac <jaisleen.sachdeva@hl.co.uk>
2023-02-20 10:33:19 +0000

    Task 1 to 5 completed

--- src/main/java/a_introduction/Task1.java
+++ src/main/java/a_introduction/Task1.java
@@ -1,4 +1,16 @@
 package a_introduction;
 
 public class Task1 {
+    public static void main(String[] args) {
+        int a = 10;
+        char b = 'c';
+        double c = 3.14159;
+        long d = 1000000000L;
+        boolean e = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
 }
--- src/main/java/a_introduction/Task5.java
+++ src/main/java/a_introduction/Task5.java
@@ -1,4 +1,17 @@
 package a_introduction;
 
 public class Task5 {
+    public static void main(String[] args) {
+        String name = "Jess";
+        int age = 38;
+
+        String message = name + ", is " + age + " years old and is ";
+        if (age < 18) {
+            message += "underage";
+        } else {
+            message += "not underage";
+        }
+        System.out.println(message);
+    }
 }
+
