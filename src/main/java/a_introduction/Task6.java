adf02e35d901c2805f57d71a820bf6287da720c2
hjaissac <jaisleen.sachdeva@hl.co.uk>
2023-02-20 11:05:56 +0000

    Task 6 and 7 completed

--- src/main/java/a_introduction/Task6.java
+++ src/main/java/a_introduction/Task6.java
@@ -1,4 +1,9 @@
 package a_introduction;
 
 public class Task6 {
+    public static void main(String[] args) {
+        double alcoholContent = -1;
+        String beerType = (alcoholContent >= 4) ? "strong" : "normal";
+        System.out.println("This beer is " + beerType);
+    }
 }
--- src/main/java/a_introduction/Task7.java
+++ src/main/java/a_introduction/Task7.java
@@ -1,4 +1,33 @@
 package a_introduction;
 
 public class Task7 {
+    public static void main(String[] args) {
+        int dayOfTheWeek = 0;
+
+        switch (dayOfTheWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day of the week");
+        }
+    }
 }
